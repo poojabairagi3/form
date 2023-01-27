7b93824413ccbd659525ad69318d4ce8c519586b
poojabairagi3 <poojabairagi0301@gmail.com>
2023-01-28 01:39:22 +0530

    adding second file

--- dom.js
+++ dom.js
@@ -17,20 +17,42 @@
 // GETELEMENTBYID
 
 // console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
 // console.log(headerTitle);
-headerTitle.textContent = 'Hello';
 // headerTitle.innerText ='Goodbye';
 // console.log(headerTitle.textContent);
 // console.log(headerTitle.innerText);
 // headerTitle.innerHTML ='<h3>Hello<h3>';
-headerTitle.style.borderBottom= 'solid 3px black';
-headerTitle.innerHTML ='<strong>Hello</strong>';
-headerTitle.style.color= 'green';
+// headerTitle.style.borderBottom= 'solid 3px black';
+// headerTitle.innerHTML ='<strong>Hello</strong>';
+// headerTitle.style.color= 'green';
 
 
 // GETELEMENTBYCLASSNAME //
+
+var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello 2'
+// items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='yellow';
+items[2].style.backgroundColor='green';
+
+// gives error
+
+// items[1].style.backgroundolor='#f4f4f4';
+
+// for (var i=0; i<items.length; i++){
+//     items[i].style.backgroundColor='#f4f4f4'; 
+
+// }
+
+for (var i=0; i<items.length; i++){
+    items[i].style.fontWeight='bold'; 
+    
+     }
+
 
 
 
