395dc79b35aafe341f77669f297e5b22fea223f6
poojabairagi3 <poojabairagi0301@gmail.com>
2023-01-28 19:01:02 +0530

    fifth commit

---
+++ fifthfile.js
@@ -0,0 +1,24 @@
+// QUERYSELECTOR//
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom ='solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value='Hello World' 
+
+// var submit= document.querySelector('input[type="submit"]');
+// submit.value="SEND"
+
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = "red";
+ 
+// var item = document.querySelector('.list-group-item:last-child');
+// item.style.color = "blue";
+
+var item = document.querySelector('.list-group-item:nth-child(2)');
+// // item.style.color = "coral";
+item.style.backgroundColor="green"
+
+var item = document.querySelector('.list-group-item:nth-child(3)');
+item.style.display="None";
